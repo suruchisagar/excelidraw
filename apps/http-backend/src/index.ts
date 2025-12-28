a4749d7e06745e49d540dc7c48ebc522414f1c44
Suruchi Sagar <suruchisagar24@gmail.com>
2025-12-28 21:48:45 +0530

    added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -9,14 +9,33 @@ const app = express();
 //signup
 app.post("/signup",(req, res)=>{
 
-    const data= CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+    const parsedData= CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.json({
             "message":"Incorrect inputs"
         })
         return;
     }
 
+    try{
+        prismaClient.user.create({
+        data: {
+            email:parsedData.data?.username,
+            password: parsedData.data.password,
+            name: parsedData.data.name
+        }
+    })
+    res.json({
+        userId: "123"
+    })
+
+    }
+    catch(e){
+        res.status(411).json({
+            message: "User already exists with this username"
+        })
+
+    }
     //db call
 
     res.json({
